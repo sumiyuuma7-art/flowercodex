1835376e869e4ed463429051ca2a672294f96cb8
nyan <>
2026-04-15 00:23:02 +0900

    add 20 pexels images and update source ledger

--- data/flower_image_overrides.js
+++ data/flower_image_overrides.js
@@ -4,7 +4,29 @@ window.FLOWER_IMAGE_OVERRIDES = window.FLOWER_IMAGE_OVERRIDES || {
   // value: image path from flowercodex.html
   //
   // Example:
-  // "1": "images/flowers/rose-01.jpg",
-  // "2": "images/flowers/sunflower-01.jpg",
-  // "3": "images/flowers/cherry-01.jpg"
+  // "1": "images/flowers/001.jpg",
+  // "2": "images/flowers/002.jpg",
+  // "3": "images/flowers/003.jpg"
+
+  // Batch placeholders for first 20 flowers
+  "1": "images/flowers/001.jpg",
+  "2": "images/flowers/002.jpg",
+  "3": "images/flowers/003.jpg",
+  "4": "images/flowers/004.jpg",
+  "5": "images/flowers/005.jpg",
+  "6": "images/flowers/006.jpg",
+  "7": "images/flowers/007.jpg",
+  "8": "images/flowers/008.jpg",
+  "9": "images/flowers/009.jpg",
+  "10": "images/flowers/010.jpg",
+  "11": "images/flowers/011.jpg",
+  "12": "images/flowers/012.jpg",
+  "13": "images/flowers/013.jpg",
+  "14": "images/flowers/014.jpg",
+  "15": "images/flowers/015.jpg",
+  "16": "images/flowers/016.jpg",
+  "17": "images/flowers/017.jpg",
+  "18": "images/flowers/018.jpg",
+  "19": "images/flowers/019.jpg",
+  "20": "images/flowers/020.jpg"
 };
